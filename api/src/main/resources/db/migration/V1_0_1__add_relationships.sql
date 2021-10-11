c9fa6e815cdd7a76b0ef25997845d7dedae787e3
Volodymyr Holiev <vovagolev23@gmail.com>
2021-10-11 17:53:16 +0300

    Correction

--- api/src/main/resources/db/migration/V1_0_1__add_relationships.sql
+++ api/src/main/resources/db/migration/V1_0_1__add_relationships.sql
@@ -1,8 +1,3 @@
-ALTER TABLE medicines ADD COLUMN price_id bigint;
-ALTER TABLE medicines ADD COLUMN pharmacy_id bigint;
-ALTER TABLE medicines ADD CONSTRAINT fk_medicine FOREIGN KEY(price_id,pharmacy_id)
-            REFERENCES prices(medicine_id,pharmacy_id);
-ALTER TABLE pharmacies ADD COLUMN medicine_id bigint;
-ALTER TABLE pharmacies ADD CONSTRAINT  fk_medicine FOREIGN KEY(medicine_id) REFERENCES medicines(id);
+
 ALTER TABLE prices ADD CONSTRAINT  fk_pharmacy FOREIGN KEY(pharmacy_id) REFERENCES pharmacies(id);
 ALTER TABLE prices ADD CONSTRAINT  fk_medicine FOREIGN KEY(medicine_id) REFERENCES medicines(id);
