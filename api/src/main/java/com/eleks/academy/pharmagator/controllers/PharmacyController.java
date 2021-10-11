b8e6c83d18fccb8f1bb1db1980007382fd76c3d6
Volodymyr Holiev <vovagolev23@gmail.com>
2021-10-11 12:42:13 +0300

    Added CRUD endpoints to PharmacyController

--- api/src/main/java/com/eleks/academy/pharmagator/controllers/PharmacyController.java
+++ api/src/main/java/com/eleks/academy/pharmagator/controllers/PharmacyController.java
@@ -5,10 +5,10 @@ import com.eleks.academy.pharmagator.repositories.PharmacyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,5 +19,45 @@ public class PharmacyController {
     @GetMapping
     public ResponseEntity<List<Pharmacy>> getAll() {
         return ResponseEntity.ok(pharmacyRepository.findAll());
+    }
+
+    @GetMapping("/getById")
+    public ResponseEntity<Pharmacy> getById(@RequestParam Long id) {
+        return ResponseEntity.of(pharmacyRepository.findById(id));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody Pharmacy pharmacy) {
+        return ResponseEntity.ok(pharmacyRepository.save(pharmacy));
+    }
+
+
+    @GetMapping("/deleteById")
+    public ResponseEntity<?> deleteById(@RequestParam Long id) {
+        Optional<Pharmacy> optionalPharmacy = pharmacyRepository.findById(id);
+        if (optionalPharmacy.isPresent()) {
+            pharmacyRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    @PostMapping("/update")
+    public ResponseEntity<?> update(@RequestBody Pharmacy pharmacy) {
+        long id = pharmacy.getId();
+        Optional<Pharmacy> optionalById = pharmacyRepository.findById(id);
+        if (optionalById.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            Pharmacy pharmacyById = optionalById.get();
+            String updateForName = pharmacy.getName();
+            pharmacyById.setName(updateForName);
+            String updateForMedicineLinkTemplate = pharmacy.getMedicineLinkTemplate();
+            pharmacyById.setMedicineLinkTemplate(updateForMedicineLinkTemplate);
+            pharmacyRepository.save(pharmacyById);
+            return ResponseEntity.ok().build();
+        }
     }
 }
