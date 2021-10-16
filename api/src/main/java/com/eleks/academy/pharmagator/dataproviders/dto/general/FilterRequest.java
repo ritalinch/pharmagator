f90ebe2c4ea65b972aabe5871107bb938fe7bc1f
ritalinch <ritalincheg@gmail.com>
2021-10-16 23:58:10 +0200

    add DS data provider and both DS and Slav DTOs

---
+++ api/src/main/java/com/eleks/academy/pharmagator/dataproviders/dto/general/FilterRequest.java
@@ -0,0 +1,12 @@
+package com.eleks.academy.pharmagator.dataproviders.dto.ds;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+public class FilterRequest {
+	private Long page;
+	private Long per;
+}
---
+++ api/src/main/java/com/eleks/academy/pharmagator/dataproviders/dto/slav/SlavMecicinePrice.java
@@ -0,0 +1,2 @@
+package com.eleks.academy.pharmagator.dataproviders.dto.slav;public class SlavMecicinePrice {
+}
