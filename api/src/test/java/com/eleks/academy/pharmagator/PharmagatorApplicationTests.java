69ce7ca32e6f5ade61b726ee5f079c8b112f1f70
Marharyta Levtsun <88599126+ritalinch@users.noreply.github.com>
2021-10-26 10:40:33 +0200

    Update PharmagatorApplicationTests.java

--- api/src/test/java/com/eleks/academy/pharmagator/PharmagatorApplicationTests.java
+++ api/src/test/java/com/eleks/academy/pharmagator/PharmagatorApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(locations = "/application-test.yaml")
 class PharmagatorApplicationTests {
 
 	@Test
