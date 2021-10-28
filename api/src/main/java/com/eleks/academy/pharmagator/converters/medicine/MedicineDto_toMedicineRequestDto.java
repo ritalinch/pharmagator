d1e8ea130f9ea15cdf06820ec477b5c8e2ecabe1
ritalinch <ritalincheg@gmail.com>
2021-10-28 11:01:49 +0200

    apply converters to Scheduler
    store Medicine DTOs to database

---
+++ api/src/main/java/com/eleks/academy/pharmagator/converters/medicine/MedicineDto_toMedicineRequestDto.java
@@ -0,0 +1,13 @@
+package com.eleks.academy.pharmagator.converters.medicine;
+
+import com.eleks.academy.pharmagator.dto.MedicineDto;
+import com.eleks.academy.pharmagator.requestEntities.MedicineRequestDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MedicineDto_toMedicineRequestDto implements MedicineDto_toRequestDtoConverter<MedicineRequestDto> {
+    @Override
+    public MedicineRequestDto get(MedicineDto dto) {
+        return new MedicineRequestDto(dto.getTitle());
+    }
+}
