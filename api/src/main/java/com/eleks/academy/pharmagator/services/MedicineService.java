d9859e30869c06946800ac2b1e2c6e53cb0822a7
ritalinch <ritalincheg@gmail.com>
2021-11-02 18:44:42 +0100

    Fix "delete" methods in controllers and services

--- api/src/main/java/com/eleks/academy/pharmagator/services/MedicineService.java
+++ api/src/main/java/com/eleks/academy/pharmagator/services/MedicineService.java
@@ -22,4 +22,27 @@ public interface MedicineService {
 
     void deleteById(Long id);
 
+<<<<<<< HEAD
+=======
+    @Override
+    public void create(MedicineRequestDto medicineRequestDto) {
+        if (!medicineRepository.existsByTitle(medicineRequestDto.getTitle())) {
+            medicineRepository.save(medicineRequestDto.getMappedEntity());
+        }
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        if (medicineRepository.existsById(id)) {
+            medicineRepository.deleteById(id);
+        }
+    }
+
+    @Override
+    public ResponseEntity<MedicineResponseDto> update(Long id, MedicineRequestDto entity) {
+        return this.medicineRepository.findById(id)
+                .map(m -> ResponseEntity.ok(entity.getMappedDtoEntity()))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+>>>>>>> 6efd804 (Fix "delete" methods in controllers and services)
 }
--- api/src/main/java/com/eleks/academy/pharmagator/services/PharmacyService.java
+++ api/src/main/java/com/eleks/academy/pharmagator/services/PharmacyService.java
@@ -4,6 +4,7 @@ import com.eleks.academy.pharmagator.dataproviders.dto.input.PharmacyDto;
 import com.eleks.academy.pharmagator.entities.Pharmacy;
 
 import java.util.List;
+<<<<<<< HEAD
 import java.util.Optional;
 
 public interface PharmacyService {
@@ -17,5 +18,49 @@ public interface PharmacyService {
     Optional<Pharmacy> update(Long id, PharmacyDto pharmacyDto);
 
     void deleteById(Long id);
+=======
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PharmacyService implements ControllerService<Long, PharmacyRequestDto, PharmacyResponseDto> {
+
+    private final PharmacyRepository pharmacyRepository;
+
+    @Override
+    public List<PharmacyResponseDto> getAll() {
+        return pharmacyRepository
+                .findAll()
+                .stream()
+                .map(PharmacyResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public ResponseEntity<PharmacyResponseDto> getById(Long id) {
+        return pharmacyRepository.findById(id)
+                .map(pharmacy -> ResponseEntity.ok(new PharmacyResponseDto(pharmacy)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @Override
+    public void create(PharmacyRequestDto pharmacyRequestDto) {
+        pharmacyRepository.save(pharmacyRequestDto.getMappedEntity());
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        if (pharmacyRepository.existsById(id)) {
+            pharmacyRepository.deleteById(id);
+        }
+    }
+
+    @Override
+    public ResponseEntity<PharmacyResponseDto> update(Long id, PharmacyRequestDto entity) {
+        return this.pharmacyRepository.findById(id)
+                .map(p -> ResponseEntity.ok(entity.getMappedDtoEntity()))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+>>>>>>> 6efd804 (Fix "delete" methods in controllers and services)
 
 }
