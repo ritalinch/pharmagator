94bb74c3a392e46ddb7d7d796b32daa548f4065c
Volodymyr Holiev <vovagolev23@gmail.com>
2021-12-04 03:11:59 +0200

    Added PharmacySocialnaDataProviderTest

---
+++ api/src/test/java/com/eleks/academy/pharmagator/dataproviders/PharmacySocialnaDataProviderTest.java
@@ -0,0 +1,65 @@
+package com.eleks.academy.pharmagator.dataproviders;
+
+import com.eleks.academy.pharmagator.dataproviders.dto.MedicineDto;
+import com.eleks.academy.pharmagator.dataproviders.dto.socialna.parsers.SocialnaParser;
+import com.eleks.academy.pharmagator.exceptions.PharmagatorApiException;
+import com.eleks.academy.pharmagator.services.ImportService;
+import com.eleks.academy.pharmagator.services.ImportServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+class PharmacySocialnaDataProviderTest {
+
+    private PharmacySocialnaDataProvider subject;
+
+    private SocialnaParser socialnaParserMock;
+
+    @BeforeEach
+    void setUp() {
+        socialnaParserMock = mock(SocialnaParser.class);
+
+        when(socialnaParserMock.getMedicinesFromPageByUrl(anyString()))
+                .thenReturn(Stream.of(
+                        MedicineDto.builder()
+                                .pharmacyName("socialna")
+                                .build()
+                ));
+
+        subject = new PharmacySocialnaDataProvider(socialnaParserMock);
+
+        ReflectionTestUtils.setField(subject, "pagesLimit", 1);
+
+        ReflectionTestUtils.setField(subject, "pharmacySocialnaBaseUrl", "https://1sa.com.ua/medikamenty");
+
+    }
+
+    @Test
+    void loadData_ok() {
+        List<MedicineDto> medicineDtos = subject.loadData().toList();
+
+        assertEquals(1, medicineDtos.size());
+
+        MedicineDto medicineDto = medicineDtos.get(0);
+
+        assertEquals("socialna", medicineDto.getPharmacyName());
+    }
+
+    @Test
+    void loadData_PharmagatorApiException(){
+        when(socialnaParserMock.getMedicinesFromPageByUrl(anyString()))
+                .thenThrow(new PharmagatorApiException("error"));
+
+        ImportService importServiceMock = mock(ImportServiceImpl.class);
+
+        assertThrows(PharmagatorApiException.class, () -> subject.loadData().forEach(importServiceMock::storeToDatabase));
+    }
+
+}
